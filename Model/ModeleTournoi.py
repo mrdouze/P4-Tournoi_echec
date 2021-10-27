0fecc1b06b6c0ba5d63dd071e8f9c901ba33bc96
mrdouze <88675369+mrdouze@users.noreply.github.com>
2021-10-27 12:21:48 +0200

    maj

---
+++ Model/ModeleTournoi.py
@@ -0,0 +1,12 @@
+
+
+class Tournoi:
+    def __init__(self, Nom, Lieu, Date, NombreDeTour=4, Tournee, Joueur,ControleDeTemps, Description)
+        self.Nom=Nom
+        self.Lieu=Lieu
+        self.Date=Date
+        self.NombreDeTour=NombreDeTour
+        self.Tournee=Tournee
+        self.Joueur=Joueur
+        self.controleDeTemps=ControleDeTemps
+        self.Description=Description
